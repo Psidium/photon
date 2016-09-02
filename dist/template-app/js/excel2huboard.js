ef6cf25e89f9e2bae35e3216183a1e05e12480e1
Gabriel Borges <gabriel.borges@sap.com>
2016-09-02 18:48:54 -0300

    add screen to populate

--- dist/template-app/js/excel2huboard.js
+++ dist/template-app/js/excel2huboard.js
@@ -26,12 +26,36 @@ if (!worksheet || worksheet.length <= 0) {
     })
 }
 var currentSheet = 0
+var headerRow = 0
+var headerConverter = ["Title", "Data Specific (issue content)", "Column", "Label"];
+var selectHeaderFunctionHTML = "";
+headerConverter.forEach(function populateHeaderConverter(data, index) {
+    selectHeaderFunctionHTML += "<option value="+index+">"+data+"</option>\n"
+})
+selectHeaderFunctionHTML = selectHeaderFunctionHTML.replace(">Label</", " selected>Label</")
+
+function updateHeaders() {
+    $(".generated-data").remove()
+    worksheet[currentSheet].data[headerRow].forEach(function forEachHeader(data, index) {
+        $("#selectors").append(
+            "<div class=\"selector-box generated-data\">\n"+
+                "<h1 class=\"title\">"+data+"</h1>\n"+
+                    "<select id=\"header"+index+"\" value=\""+index+"\" class=\"form-control\">\n"+
+                        selectHeaderFunctionHTML +
+                    "</select>\n"+
+                "</h1>\n"+
+            "</div>\n"
+        )
+    })
+}
 
 $(document).ready(function(){
     worksheet.forEach(function worksheetForEach(data, index) {
         $("#sheets").append("<option value="+ index +">"+ data.name +"</option>");
     })
+    updateHeaders()
     $("#sheets").change(function() {
         currentSheet = this.value
+        updateHeaders()
     })
 })
